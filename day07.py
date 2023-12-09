add4ca3ea9b45073d6d35c5ec91128f35466b53d
Daniel Siemssen <daniel@dr-siemssen.de>
2023-12-09 07:23:49 +0100

    Day 07

--- day07.py
+++ day07.py
@@ -1,10 +1,10 @@
-def parse(line: str, jokers: bool = False) -> tuple:
+def parse(line: str, jokers: bool = False) -> tuple[int, str, int]:
     hand, bid = line.split()
     hand = hand.translate(str.maketrans("TJQKA", ("ABCDE", "A0CDE")[jokers]))
-    hand = hand.replace("0", max(hand, key=hand.count))
-    value = sum(map(hand.count, hand))
-    return value, hand, bid
+    best_hand = hand.replace("0", max(hand, key=hand.replace("0", "").count))
+    value = sum(map(best_hand.count, best_hand))
+    return value, hand, int(bid)
 
 
 for part in zip(*((parse(line), parse(line, jokers=True)) for line in open(0))):
-    print(sum(rank * int(bid) for rank, (_best, _hand, bid) in enumerate(sorted(part), 1)))
+    print(sum(rank * bid for rank, (_value, _hand, bid) in enumerate(sorted(part), 1)))
